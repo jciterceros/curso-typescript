083efaa4ec31ba3926864f476880313e83ae04ea
jciterceros <jciterceros@yahoo.com>
2026-05-09 13:13:42 -0300

    chore: add request logging and test type-check configuration

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 import ticketsRoutes from "./routes/tickets.routes.js";
 import usersRoutes from "./routes/users.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
+const httpLogFormat = process.env.NODE_ENV === "test" ? "tiny" : "combined";
+
+app.use(morgan(httpLogFormat));
 app.use(express.json());
 
 app.use("/tickets", ticketsRoutes);
